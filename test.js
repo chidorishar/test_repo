0d510e3878a209578754a59f535a1f92ae6772ce
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-03-23 23:11:59 +0200

    ghhg

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Megapolis notifications for registrar working space
 // @namespace    http://tampermonkey.net/
-// @version      4.0
+// @version      5.0
 // @description  corpo shit
 // @author       Aga
 // @include      https://sed.ugv.corp/
